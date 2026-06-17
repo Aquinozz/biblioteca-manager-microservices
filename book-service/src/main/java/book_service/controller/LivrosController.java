41c4b435c0662d8695c0f8c47bd043cfedb7efa5
Muris <muriloaquinotrab@gmail.com>
2026-06-16 23:44:30 -0300

    Adcionando docker

--- book-service/src/main/java/book_service/controller/LivrosController.java
+++ book-service/src/main/java/book_service/controller/LivrosController.java
@@ -11,7 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import vendas_service.dto.AtualizarEstoqueRequest;
+import book_service.dto.AtualizarEstoqueRequest;
 
 import java.util.List;
 
