255dda35728528faa477e34219588b3a030b79b9
Aquinozz <muriloaquinotrab@gmail.com>
2026-07-24 17:32:28 -0300

    Adiciona botao Authorize (JWT) no Swagger de book e vendas
    - Adiciona security scheme bearerAuth no SwaggerConfig de book-service
    - Adiciona security scheme bearerAuth no SwaggerConfig de vendas-service
    - Agora todos os 3 servicos tem o botao Authorize no Swagger

--- vendas-service/src/main/java/vendas_service/config/SwaggerConfig.java
+++ vendas-service/src/main/java/vendas_service/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package vendas_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(new Server().url("http://localhost:8080"));
+                .addServersItem(new Server().url("http://localhost:8080"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
